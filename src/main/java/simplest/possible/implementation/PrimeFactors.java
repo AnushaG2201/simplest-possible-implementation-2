d0f80df94cec6bda0ba92e192a5d8cb1d394b2ae
AnushaG2201 <76487202+AnushaG2201@users.noreply.github.com>
2021-01-04 17:48:45 +0530

    Create PrimeFactors.java

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,14 @@
+package simplest.possible.implementation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public static List<Integer> primeFactorDisplay(int i) {
+        List<Integer> result;
+        if(i==1)
+        result=new ArrayList<>();
+        else
+        result= Arrays.asList(i);
+    return result;
+    }
+    }
