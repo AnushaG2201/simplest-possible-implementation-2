921c69677c9f22d652c2990fbe6c92da28f1053d
AnushaG2201 <76487202+AnushaG2201@users.noreply.github.com>
2021-01-04 18:49:22 +0530

    Update PrimeFactors.java

--- src/main/java/simplest/possible/implementation/PrimeFactors.java
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
-    public static List<Integer> primeFactorDisplay(int i) {
+    public static List<Integer> of(int i) {
         List<Integer> result;
         if(i==1)
         result=new ArrayList<>();
